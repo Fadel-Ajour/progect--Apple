3cd87e6e9a69ca73ba5fb62f1559938e29f68a58
Fadel Ajour <129401717+fadel31@users.noreply.github.com>
2024-06-13 13:22:20 +0300

    first Commit

---
+++ main.js
@@ -0,0 +1,8 @@
+let container = document.querySelector(".container")
+let img_phone = document.querySelector(".img_phone")
+function phons(src){
+    img_phone.src = src
+}
+function colors(color){
+    container.style.background = color 
+}
